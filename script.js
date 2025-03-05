923a37598081e523023220db2d2fef48917e48e9
Leonardo S. Viana <leosiqueiraviana@gmail.com>
2025-03-04 23:52:08 -0300

    Update script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('getWeatherBtn').addEventListener('click', function(){
+    //Aqui vamos adicionar o código para buscar os dados da API
+    console.log('Botão clicado!');
+});
