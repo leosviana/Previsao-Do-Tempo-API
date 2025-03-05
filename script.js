de5a8494dd2b30af6087c3ae6846fd83c6862cce
Leonardo S. Viana <leosiqueiraviana@gmail.com>
2025-03-04 23:55:09 -0300

    Updte script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 document.getElementById('getWeatherBtn').addEventListener('click', function(){
     //Aqui vamos adicionar o código para buscar os dados da API
-    console.log('Botão clicado!');
+    const city = document.getElementById('cityInput').value;
+    console.log('Cidade digitada: ', city);
 });
